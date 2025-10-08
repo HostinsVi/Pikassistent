24735c433bd39b69579a1a825b20c185e5415bac
GuilhermeOliv2 <oliveirag1920@gmail.com>
2025-10-08 19:29:12 -0300

    adcionado chatbot nas rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/landingPage/landingPage'
+import Chatbot from './pages/chatbot'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
+        <Route path='/chatbot' element={<Chatbot/>}/>
         <Route path='*' element={<Navigate to='/' replace/>}/>
         
       </Routes>
